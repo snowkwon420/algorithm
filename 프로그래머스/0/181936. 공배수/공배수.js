6df20bced9409017cc6a300d322bb8b6e0604ebe
SoonHo,Kwon <85488522+snowkwon420@users.noreply.github.com>
2024-07-26 17:00:40 +0900

    [level 0] Title: 공배수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.js
@@ -0,0 +1,12 @@
+function solution(number, n, m) {
+    var answer = 0;
+    
+    if(number%n==0){
+        if(number%m==0){
+            answer= 1;        
+        }
+    }else{
+        answer=0;
+    }
+    return answer;
+}
